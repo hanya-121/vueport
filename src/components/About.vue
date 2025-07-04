65d0a7d0f30fafa4a98f96ce50b058bc29fd85ee
hanya-121 <chyva121@gmail.com>
2025-07-04 17:57:35 +0900

    어바웃 수정

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,16 +10,16 @@
             <!-- 텍스트 -->
             <v-col cols="12" md="6" class="clinic-text">
                 <ul class="clinic_list">
-                <li class="font_b about_sub">1. 원장 상담 진료로 정확한 진단 <br>
+                <li class="font_b about_sub">1. 원장 상담 진료로 정확한 진단 <br/>
                 <p class="font_r about_text">원장이 직접 환자와의 상담을 주관하고, 상담실장은 이를 보완해주는 상담시스템이기 때문에 정확한
                                 진단을 받고 수술 결과를 예측할 수 있습니다.</p></li>
-                <li class="font_b about_sub">2. 절차와 주치의 개별의 진료 <br>
+                <li class="font_b about_sub">2. 절차와 주치의 개별의 진료 <br/>
                 <p class="font_r about_text">원장이 직접 수술 중 환자의 몸 상태를 확인하기 때문에, 
                     수술 전 환자가 상담한 내용과 오차 없이 결과로 나타날 수 있도록 합니다.</p></li>
-                <li class="font_b about_sub">3. 하루 진료 인원 제한 <br>
+                <li class="font_b about_sub">3. 하루 진료 인원 제한 <br/>
                 <p class="font_r about_text">환자 한 분 한 분에 대한 충분한 진료 시간의 확보가 곧 좋은 치료결과로 연결된다는 신념 아래, 
                     환자만족도가 높은 진료서비스를 진행하고 있습니다.</p></li>
-                <li class="font_b about_sub">4. 가장 안전하고 필요한 수술만 <br>
+                <li class="font_b about_sub">4. 가장 안전하고 필요한 수술만 <br/>
                 <p class="font_r about_text">반드시 필요한 수술만, 가장 안전한 조건 하에서 검증된 방식으로 수술하는 것이 원칙입니다. 
                     미드림 성형외과에서는 비록 시간이 걸리더라도 한 명 한 명에게 최선을 다하는 진료를 하고 있습니다.</p></li>
                 </ul>
@@ -69,6 +69,7 @@
     <!-- 운영 시간 섹션 -->
     <v-container fluid class="contact_sec">
         <v-row justify="center">
+            <v-col cols="12" class="title_font contact_title">오시는 길</v-col>
             <v-col cols="12" class="contact_img"></v-col>
         </v-row>
     </v-container>
@@ -126,31 +127,31 @@
             <v-col cols="12" md="6">
                 <!-- 자가용 -->
                 <div class="transport-block">
-                <h3 class="transport-title font_b">자가용</h3>
-                <p class="desc font_r">
-                    빌딩 내 무료 주차 가능<br>
-                    일반 승용차는 상시 가능하나 SUV차량은 주차 불가능<br>
-                    에메랄드 약국 골목에서 우회전 하시면 주차장 입구가 보입니다.
-                </p>
+                    <h3 class="transport-title font_b">자가용</h3>
+                    <p class="desc font_r">
+                        빌딩 내 무료 주차 가능<br/>
+                        일반 승용차는 상시 가능하나 SUV차량은 주차 불가능<br/>
+                        에메랄드 약국 골목에서 우회전 하시면 주차장 입구가 보입니다.
+                    </p>
                 </div>
 
                 <!-- 비행기 -->
                 <div class="transport-block mt-15 air">
-                <h3 class="transport-title font_b">비행기</h3>
-                <span class="desc font_r label-box sky">인천공항</span>
-                <p class="desc font_r">
-                    제1여객터미널/제2여객터미널 > 1층 4번 승차장<br>
-                    지하1층 15번 승차장 > 6009번 리무진 버스 탑승 > 신사역에서 하차
-                </p>
-                <span class="desc font_r label-box sky">김포공항</span>
-                <p class="desc font_r">
-                    공항터미널까지 셔틀버스 이용 > 택시 혹은 버스로<br>
-                    강남 신사동 사거리까지 이동
-                </p>
-                <span class="desc font_r label-box beige">지하철 9호선</span>
-                <p class="desc font_r">
-                    이용 시 고속터미널역에서 3호선으로 환승 > 신사역 하차
-                </p>
+                    <h3 class="transport-title font_b">비행기</h3>
+                    <span class="desc font_r label-box sky">인천공항</span>
+                    <p class="desc font_r">
+                        제1여객터미널/제2여객터미널 > 1층 4번 승차장<br/>
+                        지하1층 15번 승차장 > 6009번 리무진 버스 탑승 > 신사역에서 하차
+                    </p>
+                    <span class="desc font_r label-box sky">김포공항</span>
+                    <p class="desc font_r">
+                        공항터미널까지 셔틀버스 이용 > 택시 혹은 버스로<br/>
+                        강남 신사동 사거리까지 이동
+                    </p>
+                    <span class="desc font_r label-box beige">지하철 9호선</span>
+                    <p class="desc font_r">
+                        이용 시 고속터미널역에서 3호선으로 환승 > 신사역 하차
+                    </p>
                 </div>
             </v-col>
         </v-row>

6674a6f74aa846a20757244398b957e1507379ac
yeon-11 <wldus4660@naver.com>
2025-07-04 16:47:09 +0900

    동안성형 수술정보 제거 ~

--- src/components/Face.vue
+++ src/components/Face.vue
@@ -3,7 +3,6 @@
     <Question2/>
     <LiftingTabs/>
     <LiftingChart/>
-    <SurgeryInfo2/>
     <Precautions2/>
   </div>
 </template>
@@ -13,6 +12,5 @@
 import Question2 from '@/components/Question2.vue'
 import LiftingTabs from '@/components/LiftingTabs.vue'
 import LiftingChart from '@/components/LiftingChart.vue'
-import SurgeryInfo2 from '@/components/SurgeryInfo2.vue'
 import Precautions2 from '@/components/Precautions2.vue'
 </script>
